36167857664edb2aa0c511873eb1cb3016185a1a
criscreates <cristianmoretto25@yahoo.ca>
2025-06-15 23:40:57 -0400

    Add stripped main.cc file, clarify that source code can't be shared

---
+++ main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include "randomengine.h"
+#include "controller.h"
+
+using namespace std;
+
+int main(int argc, char * argv[]) {
+    cout << "Our source code cannot be made publicly available due to the university's academic integrity guidelines :(" << endl; 
+}
